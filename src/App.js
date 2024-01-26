3ffea3bd1e099c78c851936ddb2d36cfaf2f73e3
ficonoriega <federico.uis@gmail.com>
2024-01-25 23:29:35 -0500

    card finished. Starging background layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import React from 'react';
-import coffeeData from "./data.json";
+import coffeeData from "./assets/json/data.json";
 import CoffeeCard from './components/CoffeeCard';
+import BackgroundCoffee from './assets/images/bg-cafe.jpg'
 
 function App() {
 
   return(
-    <div>
-      <h2 className="title">Our Collection</h2>
-      <p className='description'>Introducing a Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
-      <ul className='coffeeList'>
-        {coffeeData.map(coffee => (
-          <CoffeeCard key={coffee.id} coffee={coffee} />
-        ))}
-      </ul>
-    </div>
+    <section className='hero'>
+      <div className='content'>
+        <h2 className="title">Our Collection</h2>
+        <p className='description'>Introducing a Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+        <ul className='coffeeList'>
+          {coffeeData.map(coffee => (
+            <CoffeeCard key={coffee.id} coffee={coffee} />
+          ))}
+        </ul>
+      </div>
+    </section>
+    
     
   );
 }
--- src/components/CoffeeCard.js
+++ src/components/CoffeeCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaStar } from "react-icons/fa6";
-//import Coffees from "../data.json";
+import { FaRegStar } from "react-icons/fa";
 
 const CoffeeCard = ({coffee}) => {
 
@@ -25,62 +25,20 @@ const CoffeeCard = ({coffee}) => {
             <p>{coffee.price}</p>
           </div>
           <div className="section2">
-            {coffee.rating === 0 ? (
-              <p> <FaStar /> No Rating</p>
-            ) : (
+            {coffee.rating === null ? (
+              <p> <FaRegStar color="grey" fontSize={24} style={{ marginRight: '8px' }}/> <span className="ratingDetails">No ratings</span></p>
+                ) : (
               <p>
-                <FaStar color="yellow" fontSize={24} style={{ marginRight: '8px' }}/> {coffee.rating} {coffee.votes && `(${coffee.votes} votes)`}
+                <FaStar color="yellow" fontSize={24} style={{ marginRight: '8px' }}/>
+                {coffee.rating} {coffee.votes && <spam className="ratingDetails">({coffee.votes} votes) </spam>}
               </p>
             )}
               <p>{coffee.available ? '' : 'Sold out'}</p>
           </div>
-          {/* <table>
-            <tr className="section1">
-              <td><h3>{coffee.name}</h3></td>
-              <td>{coffee.price}</td>
-            </tr>
-            <tr className="section2">
-            {coffee.rating === 0 ? (
-            <td> <FaStar /> No Rating</td>
-          ) : (
-            <td>
-              <FaStar color="yellow"/> {coffee.rating} {coffee.votes && `(${coffee.votes} votes)`}
-            </td>
-          )}
-            <td>{coffee.available ? '' : 'Sold out'}</td>
-            </tr>
-          </table> */}
         </div>
       }   
     </li>
   );
-
-    // <div>
-    //   {
-    //     Coffees && Coffees.map(coffee => {
-    //       return(
-    //         <div key = {coffee.id}>
-    //           {coffee.name} <br/>
-    //           {/* {coffee.image} */}
-    //           {coffee.price} <br/>
-    //           {coffee.rating} <br/>
-    //           {coffee.votes} <br/>
-    //           {coffee.popular} <br/>
-    //           {coffee.available}
-    //         </div>
-    //       )
-    //     })
-    //   }
-    // </div>
-    // const { name, image, price, rating, votes, popular, available } = props;
-    // const { name, image } = props;
-    // Coffees.map(coffee => {
-    //     return(
-    //         <div>
-    //             {coffee.name}
-    //         </div>
-    //     )
-    // } )
 }
 
 export default CoffeeCard;
